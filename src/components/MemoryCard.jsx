01a2d7552d7fba73075a697e0ffff42c74321da2
Chris Goggans <cgogg1@gmail.com>
2022-04-04 18:40:51 -0400

    got cards to actually flip

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -3,38 +3,51 @@ import './MemoryCard.css';
 
 
 class MemoryCard extends React.Component {
+    constructor() {
+        super()
+        this.state = { isFlipped: false };
+    }
+
+    clickHandler() {
+        this.setState( {
+            isFlipped: !this.state.isFlipped
+        } )
+    };
+
     render() {
        let memoryCardInnerClass = "MemoryCardInner"
         
-       if (this.state.isFlipped) {
-          return  memoryCardInnerClass += "flipped";
-        }
         
         return (
         <>
         <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
-           <div className={!this.props.isFlipped ? memoryCardInnerClass : "MemoryCardInner flipped"}>
+           <div className={
+               !this.state.isFlipped
+                ? memoryCardInnerClass 
+                : "MemoryCardInner flipped"}
+                >
 
                 <div className="MemoryCardBack">
-                    <img className="image" src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="DigitalCrafts Logo"></img>
+                    <img 
+                    className="image" 
+                    src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" 
+                    alt="DigitalCrafts Logo">
+                    </img>
                 </div>
+                
                 <div className="MemoryCardFront">
                     ∆
                 </div>
             </div>
         </div>
         </>
-        );    
+        );  
+          
     }
 
-    clickHandler() {
-        this.setState( {isFlipped: !this.state.isFlipped} )
-    };
+    
 
-    constructor() {
-        super();
-        this.state = { isFlipped: false };
-    }
+    
 }
 
 export default MemoryCard;
